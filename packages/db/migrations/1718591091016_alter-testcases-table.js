8054e7d05fd1be56d43d1b0977b55ced8153f817
grvbrk <gouravbarikofficial@gmail.com>
2024-06-18 18:18:04 +0530

    Added topic table

---
+++ packages/db/migrations/1718591091016_alter-testcases-table.js
@@ -0,0 +1,30 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+exports.shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.up = (pgm) => {
+  pgm.sql(`
+    ALTER TABLE testcases
+      ADD COLUMN description VARCHAR(240) NOT NULL,
+      ADD COLUMN solution VARCHAR(240) NOT NULL;
+  `);
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.down = (pgm) => {
+  pgm.sql(`
+    ALTER TABLE testcases
+      DROP COLUMN description,
+      DROP COLUMN solution;
+  `);
+};
---
+++ packages/db/migrations/1718592138730_alter-varchar-length-of-problems-table.js
@@ -0,0 +1,28 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+exports.shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.up = (pgm) => {
+  pgm.sql(`
+    ALTER TABLE problems
+    ALTER COLUMN description TYPE VARCHAR(1000);
+  `);
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.down = (pgm) => {
+  pgm.sql(`
+    ALTER TABLE problems
+    ALTER COLUMN description TYPE VARCHAR(240);
+  `);
+};
